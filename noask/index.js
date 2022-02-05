06c9319cf27a9d149c3f1257097bd59f13fe5a3f
todak-sun <tjsdydwn@gmail.com>
2022-02-05 21:07:06 +0900

    작업시작

---
+++ noask/index.js
@@ -0,0 +1,17 @@
+const robot = require('robotjs');
+
+console.log(robot.getMousePos());
+robot.moveMouse(1411, 636);
+
+
+// robot.setMouseDelay(2);
+
+// const TWO_PI = Math.PI * 2;
+// const SCREEN_SIZE = robot.getScreenSize();
+// const height = SCREEN_SIZE.height / 2 - 10;
+// const width = SCREEN_SIZE.width;
+
+// for (let x = 0; x < width; x++) {
+//   let y = height * Math.sin((TWO_PI * x) / width) + height;
+//   robot.moveMouse(x, y);
+// }
