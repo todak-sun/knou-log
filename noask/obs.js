a1dd075dcb105428b96244dc0b45faf48179d5d7
todak-sun <tjsdydwn@gmail.com>
2022-04-25 11:47:56 +0900

    녹화보조 프로그램추가

---
+++ noask/obs.js
@@ -0,0 +1,34 @@
+const robot = require('robotjs');
+
+console.log(`mouse pos : `, robot.getMousePos());
+console.log(robot.getScreenSize());
+
+setInterval(async () => {
+  // const endTime = new Date(`2022-04-25T07:10:00.000Z`);
+  const endTime = new Date(`2022-04-25T01:59:00.000Z`);
+  const now = new Date();
+  const done = endTime < now;
+  if (done) {
+    // 녹화 중단
+    robot.moveMouse(3335, 654);
+    robot.mouseClick();
+    await wait(3000);
+
+    // // 나가기
+    // robot.moveMouse(3342, 739);
+    // robot.mouseClick();
+    // await wait(3000);
+
+    process.exit(0);
+  } else {
+    // console.log(`현재시간 : ${now.toLocaleString()}, 종료시간 : ${endTime.toLocaleString()}`);
+  }
+}, 5000);
+
+function wait(time) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, time);
+  });
+}
